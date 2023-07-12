2157aab49a5e9db8a7c6e6c712c212b609d93876
mathiashole <joacomangino@gmail.com>
2023-07-12 11:51:04 -0300

    edit output in convertform.pl

--- convertform.pl
+++ convertform.pl
@@ -8,30 +8,36 @@ my $option = shift @ARGV;
 my $input_file = shift @ARGV;
 my $output_file = shift @ARGV;
 
-# Verificar si se proporcionaron todos los argumentos
-unless ($option && $input_file && $output_file) {
+# Check if all arguments are provided
+unless ($option && $input_file) {
     die "Uso: perl convertir.pl <-bed|-gff|-txt> archivo_entrada archivo_salida\n";
 }
 
-# Leer el archivo de entrada y realizar la conversión según la opción
+# Get the base name of the input file
+my ($output_file, $directories) = $input_file =~ /^(.*)\.[^.]+$/;
+
+# Add the .txt extension to the output name
+$output_file .= ".txt";
+
+# Read input file and perform conversion as per option
 if ($option eq "-bed") {
     convertir_bed_a_txt($input_file, $output_file);
 } elsif ($option eq "-gff") {
     convertir_gff_a_txt($input_file, $output_file);
 } elsif ($option eq "-txt") {
-    # Si la opción es -txt, se mantiene el archivo sin cambios
-    # Se puede agregar alguna verificación adicional si es necesario
-    rename($input_file, $output_file) or die "Error al renombrar el archivo: $!";
+    # If the option is -txt, the file is kept unchanged
+    # Some additional verification can be added if needed
+    rename($input_file, $output_file) or die "Error renaming the file: $!";
 } else {
-    die "Opción no válida. Debe ser -bed, -gff o -txt.\n";
+    die "Invalid option. Must be -bed, -gff, or -txt.\n";
 }
 
-# Función para convertir archivo BED a TXT
+# Function to convert BED file to TXT
 sub convertir_bed_a_txt {
     my ($input_file, $output_file) = @_;
 
-    open(my $input_fh, "<", $input_file) or die "No se puede abrir el archivo de entrada: $!";
-    open(my $output_fh, ">", $output_file) or die "No se puede abrir el archivo de salida: $!";
+    open(my $input_fh, "<", $input_file) or die "Cannot open input file: $!";
+    open(my $output_fh, ">", $output_file) or die "Cannot open output file: $!";
 
     while (my $line = <$input_fh>) {
         chomp $line;
@@ -47,12 +53,12 @@ sub convertir_bed_a_txt {
     close($output_fh);
 }
 
-# Función para convertir archivo GFF a TXT
+# Function to convert GFF file to TXT
 sub convertir_gff_a_txt {
     my ($input_file, $output_file) = @_;
 
-    open(my $input_fh, "<", $input_file) or die "No se puede abrir el archivo de entrada: $!";
-    open(my $output_fh, ">", $output_file) or die "No se puede abrir el archivo de salida: $!";
+    open(my $input_fh, "<", $input_file) or die "Cannot open input file: $!";
+    open(my $output_fh, ">", $output_file) or die "Cannot open output file: $!";
 
     while (my $line = <$input_fh>) {
         chomp $line;
