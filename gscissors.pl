67df5cd5c90cc0e94ae0ebb7da3152286723a474
mathiashole <mangino@fcien.edu.uy>
2025-04-11 11:01:12 -0300

    add: execution id format

--- gscissors.pl
+++ gscissors.pl
@@ -61,25 +61,38 @@ sub extractor {
     foreach my $coord (@coordinates) {
         # my ($name, $start, $end, $strand, $seq_name, @rest) = @$coord;
         # my ($name, $start, $end, $strand, @rest) = @$coord; # Adjust for strand
-        my ($name, $start, $end, $strand, $seq_name, @rest) = @$coord;
+        if ($coord_format eq 'idlist') {
+            $name = $coord->[0];
         if (exists $hash_sequence{$name}) {
-            my $contig = $hash_sequence{$name};
-            # my $sequence = extract_sequence($contig, $start, $end);
-            my $sequence = extract_sequence($contig, $start, $end, $strand);
-
+            my $sequence = $hash_sequence{$name};
             format_sequence(\$sequence);
-
-            unless ($flag_not_to_upper) {
-                $sequence = uc($sequence);
-            }
-
-            my $fasta_string = ">$name" . "_" . ($seq_name // '') . " [$start $end] " . join(" ", @rest) . "\n$sequence\n";
-            # my $fasta_string = ">$name" . "_" . ($strand // '') . " [$start $end] " . join(" ", @rest) . "\n$sequence\n";
-            print $output_fh $fasta_string;
+            $sequence = uc($sequence) unless $flag_not_to_upper;
+            print $output_fh ">$name\n$sequence\n";
         } else {
             warn "Warning: Sequence $name not found in FASTA file\n";
-            print $log_fh "Sequence $name\t$start\t$end not found in FASTA file\n" if $log_file;
+            print $log_fh "Sequence $name not found in FASTA file\n" if $log_file;
         }
+        } else {
+            my ($name, $start, $end, $strand, $seq_name, @rest) = @$coord;
+            if (exists $hash_sequence{$name}) {
+                my $contig = $hash_sequence{$name};
+                # my $sequence = extract_sequence($contig, $start, $end);
+                my $sequence = extract_sequence($contig, $start, $end, $strand);
+
+                format_sequence(\$sequence);
+
+                unless ($flag_not_to_upper) {
+                    $sequence = uc($sequence);
+                }
+
+                my $fasta_string = ">$name" . "_" . ($seq_name // '') . " [$start $end] " . join(" ", @rest) . "\n$sequence\n";
+                # my $fasta_string = ">$name" . "_" . ($strand // '') . " [$start $end] " . join(" ", @rest) . "\n$sequence\n";
+                print $output_fh $fasta_string;
+            } else {
+                warn "Warning: Sequence $name not found in FASTA file\n";
+                print $log_fh "Sequence $name\t$start\t$end not found in FASTA file\n" if $log_file;
+            }
+        }    
     }
 
     close($output_fh);
